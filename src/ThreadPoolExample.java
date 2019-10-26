37db9187e5cc826ce6c265484586ece94bf1d028
simon <simon.tenggren@gmail.com>
2019-10-26 17:05:29 +0200

    added .gitignore

---
+++ src/ThreadPoolExample.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPools {
+
+    public static void main(String[] args) {
+        ThreadPool tp = new ThreadPool(3);
+
+    }
+
+}
